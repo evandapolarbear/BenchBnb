38e6da349a03cf38feefef8a1d54e3945f425fa3
BendersBender <ev.l.waits@gmail.com>
2016-10-28 15:08:09 -0700

    complete backend Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :signed_in?
+
+  def currend_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def sign_out
+    current_user.try(:reset_session_token!)
+    session[:session_token] = nil
+  end
+
+  def sign_in(user)
+    session[:session_token]user.reset_session_token!
+  end
+
 end
