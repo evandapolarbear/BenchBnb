e53bc2d16882f9b7cce297ad77058b1aae879e62
BendersBender <ev.l.waits@gmail.com>
2016-10-28 14:30:01 -0700

    first commit

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let rootDiv = document.getElementById("root");
+
+  ReactDOM.render(<h1>Heyooo!</h1>, rootDiv);
+  }
+);
